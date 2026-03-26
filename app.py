bebca4bd314c91006518ef3992706e48786218fb
Nisa Nur Keş <nisanur.kes33@gmail.com>
2026-03-26 20:48:17 +0300

    canlıya alma denemesi2

--- app.py
+++ app.py
@@ -522,6 +522,7 @@ def create_app():
 app = create_app()
 
 if __name__ == "__main__":
-    # Windows ortamında doğrudan çalıştırılabilir
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    debug = os.environ.get("FLASK_DEBUG", "0") == "1"
+    app.run(host="0.0.0.0", port=port, debug=debug)
 
